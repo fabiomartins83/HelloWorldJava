386eb94e4041d1ca81a85d0e812b1991275077f8
Fábio Martins <fabio.martins.01@hotmail.com>
2020-03-28 11:36:11 -0300

    primeiro commit no Netbeans

---
+++ src/helloworldjava/HelloWorldJava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Java visual com swing import javax.swing.JFrame;  
+// classe JFrame import javax.swing.JLabel;  
+// classe JLabel public 
+package helloworldjava;
+
+/**
+ *
+ * @author Fábio de Almeida Martins
+ */
+
+import javax.swing.*;
+
+public class HelloWorldJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //JFrame frame = new JFrame();           //criando frame
+        //frame.setTitle("Hi!");                 //título do frame
+        //frame.add(new JLabel("Hello, world!"));//adicionando texto ao frame
+        //frame.pack();                          //tamanho para smallest
+        //frame.setLocationRelativeTo(null);     //entralizando frame
+        //frame.setVisible(true);                //exibindo frame
+        System.out.println("Hello World!!!");
+    }
+    
+}
